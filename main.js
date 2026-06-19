87fbf45d3d996f530deb084d44e425dd9debe36f
Edem Ahiati <itseddieahiati@gmail.com>
2026-06-19 19:38:00 +0000

    Update ui

--- main.js
+++ main.js
@@ -76,3 +76,22 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+document.querySelectorAll(".education_stage").forEach(card => {
+
+    card.addEventListener("mousemove", e => {
+
+        const rect = card.getBoundingClientRect();
+
+        card.style.setProperty(
+            "--x",
+            `${e.clientX - rect.left}px`
+        );
+
+        card.style.setProperty(
+            "--y",
+            `${e.clientY - rect.top}px`
+        );
+    });
+
+});
